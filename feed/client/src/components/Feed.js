53902cd6512cd2db77b3aaaaece6a513b7789450
Joe Boulos <jab12288@gmail.com>
2019-01-16 12:50:17 -0800

    removed "allow" prop from feed

--- feed/client/src/components/Feed.js
+++ feed/client/src/components/Feed.js
@@ -57,7 +57,7 @@ Updates:
               {/* </td> */}
 
               {/* <td> */}
-              <iframe width="560" height="315" src={`${ramFeed.videoclip}`} frameBorder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+              <iframe width="560" height="315" src={`${ramFeed.videoclip}`} frameBorder="0" allowFullScreen />
               <p>
                 {ramFeed.newsfeed}
                 <br />
